98e589d7bf2ac3dc3eed420c610881ad7f33326e
vidya inti <intividya@gmail.com>
2020-12-10 12:20:17 +0100

    js for single and multi posts

---
+++ blog-client/js/post.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    fetchData();
+}
+
+let blogPost = document.getElementById("blog-post");
+
+async function fetchData() {
+  let urlParams = new URLSearchParams(window.location.search);
+
+  try {
+    let response = await fetch(
+      "http://localhost:3000/posts/" + urlParams.get("id")
+    );
+    let data = await response.json();
+
+    let postHtml = "";
+
+    let postDate = new Date(data.date);
+      postHtml += `
+            <div>
+                <h4>${data.title}</h4>
+                <p>${data.author}</p>
+                <p>${postDate.getFullYear()}-${postDate.getMonth()}-${postDate.getDate()}</p>
+                <p>Tags: ${data.tags}</p>
+                <p class="single-post-content">${data.content}</p>
+            </div>
+            `;
+
+    blogPost.innerHTML = postHtml;
+
+  } catch(e) {
+    throw new Error(e);
+  }
+}
---
+++ blog-client/js/posts.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    fetchPosts();
+    }
+    
+    let container = document.getElementById('container');
+    let posts = document.getElementById('posts');
+    
+    async function fetchPosts() {
+        try {
+            let response = await fetch("http://localhost:3000/posts");
+            let data = await response.json();
+            let blogHTML = "";
+            for(let posts of data.reverse()) {
+            let postDate = new Date(posts.date);
+                blogHTML += `
+                    <div>
+                        <h4>${posts.title}</h4>
+                        <p> ${posts.author}</p>
+                        <p>${postDate.getFullYear()}-${postDate.getMonth()}-${postDate.getDate()}</p>
+                        <p>Tags: ${posts.tags}</p>
+                        <p class="blog-post-text">${posts.content.substring(0, 100)}</p>
+                    </div>
+                `;
+            }
+            posts.innerHTML = blogHTML;
+            
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
